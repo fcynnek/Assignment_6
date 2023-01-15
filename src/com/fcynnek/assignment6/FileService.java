3759cc1b916ce10453d5d874e27e07d94c2c3c6a
fcynnek <kchengfung@gmail.com>
2023-01-15 17:56:16 -0500

    Created POJO and started FileService

---
+++ src/com/fcynnek/assignment6/FileService.java
@@ -0,0 +1,66 @@
+package com.fcynnek.assignment6;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileService {
+// this class will read the three CSV files 
+	
+	String teslaModel3 = "model3.csv";
+	String teslaModelS = "modelS.csv";
+	String teslaModelX = "modelX.csv";
+	
+	public static Integer countLines (String fileName) {
+		
+		BufferedReader fileReader = null;
+		
+		int numberOfLines = 0;
+		
+		try {
+			fileReader = new BufferedReader(new FileReader(fileName));
+			try {
+				while ((fileReader.readLine()) != null) {
+					numberOfLines = numberOfLines + 1;
+				}
+			} catch (IOException e) {
+				System.out.println("I/O exception ocurred while reading the file");
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(".csv file error");
+			e.printStackTrace();
+		} finally {
+			try {
+				fileReader.close();
+			} catch (IOException e) {
+				System.out.println("I/O exception ocurred while closing the reader");
+				e.printStackTrace();
+			}
+		}
+		return numberOfLines;
+	}
+	
+	public ProductPOJO[] readSalesData (String fileName) {
+		
+		BufferedReader fileReader = null;
+		
+		Integer numberOfLines = countLines(fileName);
+		
+		ProductPOJO[] salesData = new ProductPOJO[numberOfLines];
+		
+		int i = 0;
+		
+		try {
+			fileReader = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			System.out.println(".csv file error");
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+		
+	}
+}
---
+++ src/com/fcynnek/assignment6/ProductPOJO.java
@@ -0,0 +1,56 @@
+package com.fcynnek.assignment6;
+
+import java.util.Date;
+
+public class ProductPOJO {
+// this class will be used to store the data from the three separate CSV files
+	
+	private Date model3_date;
+	private Long model3_sales;
+	private Date modelS_date;
+	private Long modelS_sales;
+	private Date modelX_date;
+	private Long modelX_sales;
+	
+	
+	public Date getModel3_date() {
+		return model3_date;
+	}
+	public Long getModel3_sales() {
+		return model3_sales;
+	}
+	public Date getModelS_date() {
+		return modelS_date;
+	}
+	public Long getModelS_sales() {
+		return modelS_sales;
+	}
+	public Date getModelX_date() {
+		return modelX_date;
+	}
+	public Long getModelX_sales() {
+		return modelX_sales;
+	}
+	
+	
+	public void setModel3_date(Date model3_date) {
+		this.model3_date = model3_date;
+	}
+	public void setModel3_sales(Long model3_sales) {
+		this.model3_sales = model3_sales;
+	}
+	public void setModelS_date(Date modelS_date) {
+		this.modelS_date = modelS_date;
+	}
+	public void setModelS_sales(Long modelS_sales) {
+		this.modelS_sales = modelS_sales;
+	}
+	public void setModelX_date(Date modelX_date) {
+		this.modelX_date = modelX_date;
+	}
+	public void setModelX_sales(Long modelX_sales) {
+		this.modelX_sales = modelX_sales;
+	}
+	
+	
+}
